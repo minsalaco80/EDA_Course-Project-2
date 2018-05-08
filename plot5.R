dce0d02a80e9ee0674bbe9fa77b49808e2edbbf7
minsalaco80 <minsalaco80@gmail.com>
2018-05-07 17:52:22 -0700

    Add files via upload

---
+++ plot5.R
@@ -0,0 +1,21 @@
+##5 How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore City?##
+
+unzip("exdata%2Fdata%2FNEI_data (1).zip")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+Vehicle<- grepl("vehicle", SCC$EI.Sector, ignore.case=TRUE)
+VehicleSCC<- SCC[Vehicle,]$SCC
+VehicleNEI<- NEI[NEI$SCC %in% VehicleSCC,]
+BCVehicle<- subset(VehicleNEI, fips == 24510, select = c("Emissions", "year"))
+BCVehicleTotal<- aggregate(Emissions ~ year, BCVehicle, sum)
+png(filename="plot5.png")
+plot(BCVehicleTotal$year, BCVehicleTotal$Emissions, "b", pch=16,
+			main = "Baltimore City Motor Vehicle PM2.5 Emission",
+			xlab = "Year",
+			ylab = "Emissions (t)",
+			xaxt="n", yaxt="n", ylim = c(0,400), xlim = c(1999,2008)
+			)
+axis(side=1, at=seq(1999, 2008, by=3))
+axis(side=2, at=seq(0, 400, by=100))
+dev.off()
